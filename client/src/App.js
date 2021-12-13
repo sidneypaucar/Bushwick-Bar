4569cd78938b4b42a0e096ea88c3e05be2e271a0
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-12-13 11:11:13 -0500

    useHistory added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useHistory } from 'react-router-dom'
 import Layout from './layouts/Layout';
 import SignIn from './screens/SignIn';
 import { signinUser, signupUser } from './services/auth';
@@ -8,15 +8,18 @@ import SignUp from './screens/SignUp';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
 
   const handleSignIn = async (formData) => {
     const userData = await signinUser(formData);
     setCurrentUser(userData);
+    history.push('/')
   }
 
   const handleSignUp = async (formData) => {
     const userData = await signupUser(formData);
     setCurrentUser(userData);
+    history.push('/')
   }
 
   return (
