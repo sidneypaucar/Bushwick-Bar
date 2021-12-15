04dead7b6110c320901a183be43d9d8154d7c7b4
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-12-15 13:05:08 -0500

    footer css

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,18 +1,15 @@
 import { ExternalLink } from 'react-external-link'
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './Footer.css'
 
 export default function Footer() {
   return (
     <div className='footer'>
       <footer>
-        <ExternalLink className='external-link' href='https://github.com/jckrft'><GitHubIcon />
-        </ExternalLink>
-        <ExternalLink className='external-link' href='https://github.com/alexnwarton'><GitHubIcon />
+        <ExternalLink className='external-link' href='https://www.linkedin.com/in/sidney-paucar/'><LinkedInIcon />
         </ExternalLink>
         <ExternalLink className='external-link' href='https://github.com/sidneypaucar'><GitHubIcon />
-        </ExternalLink>
-        <ExternalLink className='external-link' href='https://github.com/zachpartin'><GitHubIcon />
         </ExternalLink>
         </footer>
     </div>
