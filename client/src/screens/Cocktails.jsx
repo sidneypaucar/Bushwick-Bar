d8df0f7f84f49f9988044c72baf7f298d6433bd7
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-12-15 04:14:37 -0500

    started cocktails css

--- client/src/screens/Cocktails.jsx
+++ client/src/screens/Cocktails.jsx
@@ -5,11 +5,11 @@ import './Cocktails.css'
 export default function Cocktails({ cocktails, handleCocktailDelete, currentUser }) {
   return (
     <div className= "cocktails_background">
-      <h3>Cocktails</h3>
+      <h3 className='cocktails_title'>Cocktails</h3>
       {cocktails.map((cocktail) => (
         <div key={cocktail.id}>
-          <Link to={`/cocktails/${cocktail.id}`}>
-            <p>{cocktail.name}</p>
+          <Link className='cocktail_link' to={`/cocktails/${cocktail.id}`}>
+            <p className='cocktail_name'>{cocktail.name}</p>
           </Link>
           {currentUser && currentUser.id === cocktail.user_id && (
             <>
@@ -22,6 +22,8 @@ export default function Cocktails({ cocktails, handleCocktailDelete, currentUser
         </div>
       ))}
       <Link to='/cocktails/new'>
+        <br />
+        <br />
         <button>Create</button>
       </Link>
     </div>
