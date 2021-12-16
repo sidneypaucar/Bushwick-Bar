1c2eb1983b9fabc0ec64f51be634211acc4ddf92
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-12-16 02:28:23 -0500

    media queries

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'
 export default function Home() {
   return (
     <div className='home'>
-      <h1 className='home-header'>BUSHWICK BAR</h1>
+      <h1 className='home_header'>BUSHWICK BAR</h1>
       <h2 className='slogan'>You’re unique, and so is your drink.</h2>
     </div>
   )
