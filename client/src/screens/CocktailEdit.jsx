87e29e354c4ee9dc7e9ea8add9387b8b19484a05
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-12-15 22:49:11 -0500

    edit button css

--- client/src/screens/CocktailEdit.jsx
+++ client/src/screens/CocktailEdit.jsx
@@ -32,9 +32,13 @@ export default function CocktailEdit({ cocktails, handleCocktailUpdate }) {
       handleCocktailUpdate(id, formData);
     }}>
       <h3 className='cocktail_edit_title'>Edit Cocktail</h3>
-      <label className='cocktail_edit_name'>
-        Name:
-        <input type='text' name='name' value={name} onChange={handleChange} />
+      <label>
+          <input className='cocktail_edit_name'
+            type='text'
+            name='name'
+            value={name}
+            onChange={handleChange}
+          />
       </label>
       <button>Submit</button>
     </form>
